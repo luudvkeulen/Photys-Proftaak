0f2805aecc5b0d371083d1f29b56fb622bbf3528
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-08-31 21:58:27 +0200

    Example page added

--- Phontys/app/controllers/Application.java
+++ Phontys/app/controllers/Application.java
@@ -2,10 +2,10 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.*;
 
 public class Application extends Controller {
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(testpage.render("test"));
     }
 }
