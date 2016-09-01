ba91b490957aad859d4ce239f35d3fdb45496e58
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-09-01 16:59:01 +0200

    User model

---
+++ Phontys/app/models/User.java
@@ -0,0 +1,10 @@
+package models;
+
+public class User {
+    private String Username;
+    private String FirstName;
+    private String LastName;
+    private String Address;
+    private String Zip;
+    private Boolean Photographer;
+}
