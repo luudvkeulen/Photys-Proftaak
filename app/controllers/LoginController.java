00f4084fd2ea4f0cffe63208426acf3806bc96bd
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-09-07 22:20:32 +0200

    POST Working

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,20 +1,29 @@
 package controllers;
 
 import business.DatabaseController;
+import play.Logger;
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
+import play.data.Form;
 
 import java.sql.Connection;
 
 public class LoginController extends Controller {
     public Result index() {
-        testdb();
         return ok(login.render());
     }
 
     private void testdb() {
         DatabaseController dbc = new DatabaseController();
         Connection con = dbc.getConnection();
+    }
+
+    public Result login() {
+        DynamicForm bindedForm = Form.form().bindFromRequest();
+        Logger.info("Username is: " + bindedForm.get("username"));
+        Logger.info("Password is: " + bindedForm.get("password"));
+        return ok();
     }
 }
