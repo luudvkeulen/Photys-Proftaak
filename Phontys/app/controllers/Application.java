8ceaa0785d04f8abc23d2d05f4b8a7100ff71b39
Keulen,Luud L. van <l.vankeulen@student.fontys.nl>
2016-09-07 13:43:53 +0200

    Revert "Changed folder"
    This reverts commit 9e06707663bfb8ea0664f4a888b2acd65483be86

---
+++ Phontys/app/controllers/Application.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+public class Application extends Controller {
+    public Result index() {
+        return ok(testpage.render());
+    }
+
+    public Result nextpage() {
+        return ok(nexttestpage.render());
+    }
+}
