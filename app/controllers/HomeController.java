1c258912f700b01521b0ac1c4d1cb4cf427745c9
Bram de Boer <bramdeboer@outlook.com>
2016-09-27 15:13:12 +0200

    photo's gemaakt

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,18 @@
 package controllers;
 
+import models.Photo;
+import models.User;
+import play.db.DB;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -16,8 +26,26 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(index.render());
+    public Result index() throws SQLException {
+        Connection connection = DB.getConnection();
+        PreparedStatement statement = connection.prepareStatement("select p.*, u.first_name, u.last_name, u.emailadres, a.name as 'album_name' from picture p left join `user` u on p.photographer_id = u.id left join album a on a.id = p.album_id where album_id in (select id from album where private = 0) order by p.date desc;");
+        ResultSet result = statement.executeQuery();
+
+        List<Photo> photos = new ArrayList<>();
+        while(result.next()){
+            Photo photo = new Photo(result.getInt("id"), result.getString("name"),
+                    new User(result.getInt("photographer_id"),
+                            result.getString("first_name"),
+                            result.getString("last_name"),
+                            result.getString("emailadres")),
+                    result.getInt("file_size"),
+                    result.getDate("date"),
+                    result.getString("album_name"),
+                    result.getString("file_location"));
+            photos.add(photo);
+        }
+
+        return ok(index.render(photos));
     }
 
 }
