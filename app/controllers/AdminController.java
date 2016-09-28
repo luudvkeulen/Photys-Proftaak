786f32d219dd93f55e1f4d48645ce03701e4440b
Erybii <thijs.devries@student.fontys.nl>
2016-09-28 11:35:28 +0200

    Added ftp folder structure

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -1,12 +1,22 @@
 package controllers;
 
+import com.typesafe.config.ConfigFactory;
 import models.User;
 import models.UserType;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
 
 /**
  * Created by Thijs on 14-9-2016.
  */
 public class AdminController {
+
+    private FTPClient ftpClient;
+    private static String server = "137.74.163.54";
+    private static int port = 21;
+
+    private boolean result;
 
     public AdminController()
     {
@@ -18,7 +28,28 @@ public class AdminController {
         if(user.getUserType() == UserType.requestedPhotographer)
         {
             user.changeUserType(UserType.Photographer);
-            return true;
+            //Make directory for user on the FTP server
+            try
+            {
+                ftpClient.connect(server, port);
+                //System.out.println(ConfigFactory.load().getString("db.default.ftpPassword") + ConfigFactory.load().getString("db.default.ftpUser"));
+                ftpClient.login(ConfigFactory.load().getString("db.default.ftpUser"), ConfigFactory.load().getString("db.default.ftpPassword"));
+                ftpClient.enterLocalPassiveMode();
+
+                if(ftpClient.changeWorkingDirectory("/Photographers/" + user.getEmailAdress()))
+                {
+                    return true;
+                }
+                else
+                {
+                    ftpClient.makeDirectory("/Photographers/" + user.getEmailAdress());
+                    return true;
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
         }
         else
         {
