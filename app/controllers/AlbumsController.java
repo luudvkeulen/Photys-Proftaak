f2a9ad8170c8353c8c8b47732863c70b9da4a13b
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-10-12 10:22:46 +0200

    Removed unused controller

--- app/controllers/AlbumsController.java
+++ app/controllers/AlbumsController.java
@@ -4,9 +4,6 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
-/**
- * Created by bramd on 21-9-2016.
- */
 public class AlbumsController extends Controller {
     public Result index() {
         return ok(albums.render());
