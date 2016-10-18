8fafc7fa96654f969ef6152eb90885fa88fc744d
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-10-18 18:18:54 +0200

    New database method

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -37,8 +37,7 @@ public class HomeController extends Controller {
         //c.AddItemToCart(new OrderProduct(FilterType.brannan,new Product(0 ,"mok", "-", 2), new Photo("fotonaam", 2.0)));
 
         //System.out.println(c.GetCart());
-        db.getConnection();
-        Connection connection = DB.getConnection();
+        Connection connection = db.getConnection();
         PreparedStatement statement = connection.prepareStatement("select p.*, u.first_name, u.last_name, u.emailadres, a.name as album_name from picture p left join `user` u on p.photographer_id = u.id left join album a on a.id = p.album_id where album_id in (select id from album where private = 0) order by RAND()");
         ResultSet result = statement.executeQuery();
 
