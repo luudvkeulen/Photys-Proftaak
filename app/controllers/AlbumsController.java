58f8f5bbdccf3d3bd3e7e3625db4d7db5236255f
Wijst,Teun T.T.G. van der <t.vanderwijst@student.fontys.nl>
2016-10-19 11:50:51 +0200

    merge conflict resolved

--- app/controllers/AlbumsController.java
+++ app/controllers/AlbumsController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.typesafe.config.ConfigFactory;
 import logic.PhotographerLogic;
+import models.Album;
 import models.User;
 import play.api.Logger;
 import play.api.Play;
@@ -13,6 +14,7 @@ import play.mvc.Result;
 import views.html.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
@@ -31,32 +33,53 @@ public class AlbumsController extends Controller {
     }
 
     //Gets all albums that the user with userID is allowed to look at
-    private List<Album> GetAllAlbums(int userID) {
-        return null;
-    }
-
-    private int GetUserID(String email) {
-        int userID;
+    private List<Album> GetAllAlbums(int userID)
+    {
+        List<Album> albums;
         Connection connection = DB.getConnection();
         PreparedStatement statement = null;
+
         try
         {
-            statement = connection.prepareStatement("INSERT INTO ALBUM (`name`, `photographer_id`, `description`, `private`, `AlbumURL`) VALUES (?,?,?,?,?)");
-            statement.setString(1, name);
-            statement.setInt(2, PhotographerLogic.findPhotographerId(session("user")));
-            statement.setString(3, description);
-            statement.setInt(4, privateAlbum);
-            statement.setString(5, GenerateAlbumURL());
-
-            Boolean result = statement.execute();
-            connection.close();
-            return result;
-
+            statement = connection.prepareStatement("SELECT `album_id` FROM `useralbum` WHERE `user_id` = ?");
         }
         catch(SQLException e)
         {
             e.printStackTrace();
-            return false;
         }
+        return null;
+    }
+
+    private int GetUserID(String email)
+    {
+        int userID;
+        Connection connection = DB.getConnection();
+        PreparedStatement statement = null;
+
+        try
+        {
+            statement = connection.prepareStatement("SELECT `id` FROM `user` WHERE `email` = ?");
+            statement.setString(1, email);
+
+            ResultSet result = statement.executeQuery();
+            userID = result.getInt("id");
+            connection.close();
+
+            return userID;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    private void GetAvailableAlbums()
+    {
+        //Get the user id
+        GetUserID(session("user"));
+        //Get all album id's that are available for the user with user id
+
+        //Make albums
     }
 }
