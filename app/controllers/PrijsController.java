92e1011a8cd682628ec65a7484d7178fbb131056
Erybii <thijs.devries@student.fontys.nl>
2016-11-08 10:51:57 +0100

    Added PrijsController Added CalcTotalPrice method

---
+++ app/controllers/PrijsController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.Product;
+import play.mvc.Controller;
+import play.db.Database;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+/**
+ * Created by Thijs on 8-11-2016.
+ */
+public class PrijsController extends Controller {
+
+    private  Database db;
+
+
+    @Inject
+    public PrijsController(Database db)
+    {
+        this.db = db;
+    }
+
+    public double CalcTotalPrice(ArrayList<Product> productsInCart)
+    {
+
+        double totalPrice = 0;
+
+        for(Product p : productsInCart) {
+
+            totalPrice += p.getPrice();
+        }
+
+        return totalPrice;
+    }
+
+}
