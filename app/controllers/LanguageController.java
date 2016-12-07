172b60d96dc60a8158624bc35afcaa2b434e5768
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-12-07 13:06:44 +0100

    Translation framework added. Only need to change static text to dynamic

---
+++ app/controllers/LanguageController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class LanguageController extends Controller {
+    public Result changeLangEn() {
+        Controller.changeLang("en");
+        return redirect(request().getHeader("referer"));
+    }
+
+    public Result changeLangNl() {
+        Controller.changeLang("nl");
+        return redirect(request().getHeader("referer"));
+    }
+}
---
+++ conf/messages.nl
@@ -0,0 +1,11 @@
+#main bar
+register=Registreer
+login=Inloggen
+items=Producten
+#login page
+login.title=Log in alstublieft
+login.signin=Inloggen
+login.password=Wachtwoord
+login.email=Email Adres
+#register page
+reg.title=Voer uw persoonlijke gegevens hier in om te registreren voor een account.
