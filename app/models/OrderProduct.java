ce1d0ddf4ea78cea779a47e42759075d13c0ea1b
luud <l.vankeulen@student.fontys.nl>
2017-01-17 14:13:58 +0100

    Fixed paypal again

--- app/models/OrderProduct.java
+++ app/models/OrderProduct.java
@@ -5,33 +5,26 @@ public class OrderProduct {
     private final String description;
     private final int amount;
     private final double price;
-    private final double picturePrice;
-
     public String getName() {
         return name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public int getAmount() {
         return amount;
     }
-
     public double getPrice() {
         return price;
     }
-
     public double getTotalPrice() {
-        return (price + picturePrice) * amount;
+        return price * amount;
     }
 
-    public OrderProduct(String name, String description, int amount, double price, double picturePrice) {
+    public OrderProduct(String name, String description, int amount, double price) {
         this.name = name;
         this.description = description;
         this.amount = amount;
         this.price = price;
-        this.picturePrice = picturePrice;
     }
 }
